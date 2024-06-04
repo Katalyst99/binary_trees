2b2b4ed939fa6a270e900b252ae3ac65d5442c97
Katalyst99 <katleho333@gmail.com>
2024-06-04 08:49:08 -0700

    correction of tree height func

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,10 +11,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		return (0);
-	}
 	lh = binary_tree_height(tree->left);
 	rh = binary_tree_height(tree->right);
 	if (lh >= rh)
